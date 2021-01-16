e5aca480657dce97ec1b304d6aacab39f9d6e435
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2021-01-16 12:37:24 -0800

    1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAU0vHsqrdv2HU5tKb-svDEbA_GyRFm1ts",
+    authDomain: "dkitter-8d3e5.firebaseapp.com",
+    projectId: "dkitter-8d3e5",
+    storageBucket: "dkitter-8d3e5.appspot.com",
+    messagingSenderId: "355307654561",
+    appId: "1:355307654561:web:3e1a465f7de9fb63064fe0"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
