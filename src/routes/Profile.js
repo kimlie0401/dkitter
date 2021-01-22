c50ec6081d18da25f4a2ea8b5653236ccd33fb18
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2021-01-21 17:23:08 -0800

    #2.6 Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { authService } from "../fbase";
 
-const profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
