7b7b0103596e0887fcbcb6b36f119382511f522f
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2021-01-28 16:48:05 -0800

    #3.5 Delete and Update part Two

--- src/components/Dweet.js
+++ src/components/Dweet.js
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../fbase";
 
-const Dweet = ({ dweetObj, isOwner }) => (
-  <div>
-    <h4>{dweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete Dweet</button>
-        <button>Edit Dweet</button>
-      </>
-    )}
-  </div>
-);
+const Dweet = ({ dweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newDweet, setNewDweet] = useState(dweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this dweet?");
+
+    if (ok) {
+      await dbService.doc(`dweets/${dweetObj.id}`).delete();
+      //   await dbService.collection("dweets").doc(`${dweetObj.id}`).delete();
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`dweets/${dweetObj.id}`).update({ text: newDweet });
+    setEditing(false);
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDweet(value);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your dweet"
+              value={newDweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Dweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{dweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Dweet</button>
+              <button onClick={toggleEditing}>Edit Dweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Dweet;
